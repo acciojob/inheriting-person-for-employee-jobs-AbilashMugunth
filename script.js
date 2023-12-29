c13c2617bca0bdd3fb14a186ac98fdffd230019e
Abilash <90945323+AbilashMugunth@users.noreply.github.com>
2023-12-29 14:08:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ function Person(name, age) {
 }
 
 Person.prototype.greet = function () {
-  //   return `Hello, my name is ${this.name}, I am ${this.age} years old.`;
   console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
 };
 
@@ -16,10 +15,12 @@ function Employee(name, age, jobTitle) {
 Employee.prototype = Object.create(Person.prototype);
 
 Employee.prototype.jobGreet = function () {
-  console.log(
-    `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
-  );
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 };
+
+
+
+
 
 // Do not change code below this line
 window.Person = Person;
